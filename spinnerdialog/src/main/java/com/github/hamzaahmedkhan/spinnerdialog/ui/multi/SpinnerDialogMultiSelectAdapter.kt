1ec64124bb115d70b9a0abc8ca17cb9d7489f1e8
hamzaahmedkhan <hamza_ahmed_khan@yahoo.com>
2020-06-02 14:26:33 +0500

    Convert to Kotlin

---
+++ spinnerdialog/src/main/java/com/github/hamzaahmedkhan/spinnerdialog/ui/multi/SpinnerDialogMultiSelectAdapter.kt
@@ -0,0 +1,78 @@
+package com.github.hamzaahmedkhan.spinnerdialog.ui.multi
+
+import android.app.Activity
+import android.content.Context
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.CheckBox
+import android.widget.LinearLayout
+import android.widget.TextView
+import com.github.hamzaahmedkhan.spinnerdialog.R
+import com.github.hamzaahmedkhan.spinnerdialog.callbacks.OnSpinnerItemCheckboxClickListener
+import com.github.hamzaahmedkhan.spinnerdialog.models.SpinnerModel
+import java.util.*
+
+/**
+ */
+class SpinnerDialogMultiSelectAdapter(
+    private val context: Context,
+    var arrData: ArrayList<SpinnerModel>,
+    private val onItemClick: OnSpinnerItemCheckboxClickListener
+) : RecyclerView.Adapter<SpinnerDialogMultiSelectAdapter.ViewHolder>() {
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): ViewHolder {
+        val itemView: View = LayoutInflater.from(context)
+            .inflate(R.layout.item_spinner_dialog_with_checkbox, parent, false)
+        return ViewHolder(
+            itemView
+        )
+    }
+
+    override fun onBindViewHolder(
+        holder: ViewHolder,
+        i: Int
+    ) {
+        val model =
+            arrData[holder.adapterPosition]
+        with(holder){
+            this.txtChoice.text = model.text
+            this.checkbox.isChecked = model.isSelected
+            this.contParentLayout.setOnClickListener { view: View? ->
+                this.checkbox.isChecked = !holder.checkbox.isChecked
+                model.isSelected = !model.isSelected
+                onItemClick.onItemClick(
+                    this.adapterPosition,
+                    model,
+                    this@SpinnerDialogMultiSelectAdapter
+                )
+            }
+        }
+
+    }
+
+    fun addItem(homeCategories: ArrayList<SpinnerModel>) {
+        arrData = homeCategories
+        notifyDataSetChanged()
+    }
+
+    override fun getItemCount(): Int {
+        return arrData.size
+    }
+
+    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        var checkbox: CheckBox
+        var txtChoice: TextView
+        var contParentLayout: LinearLayout
+
+        init {
+            checkbox = view.findViewById(R.id.checkbox)
+            txtChoice = view.findViewById(R.id.txtChoice)
+            contParentLayout = view.findViewById(R.id.contParentLayout)
+        }
+    }
+
+}
